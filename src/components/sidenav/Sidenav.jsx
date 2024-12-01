832055b69fbbe0b7d786d0c93591cd6d878c5b1b
HikaruXA <quaintuser321@gmail.com>
2024-12-01 16:50:58 +0800

    changes

--- src/components/sidenav/Sidenav.jsx
+++ src/components/sidenav/Sidenav.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import styles from "./sidenav.module.css";
 
 const SideNav = ({ menuItems, logo, onMenuItemClick, onClose }) => {
+  const handleMenuItemClick = (item) => {
+    // Check if the screen width is less than 768px (mobile devices)
+    if (window.innerWidth <= 768) {
+      // Close the sidenav after the menu item click
+      onClose();
+    }
+    // Call the passed function to handle menu item click
+    onMenuItemClick(item);
+  };
+
   return (
     <div className={styles.sideNavContainer}>
       <div className={styles.logoContainer}>
-        <img src="/logo/bee.svg" alt="Logo" className={styles.logo} />
+        <img src={logo} alt="Logo" className={styles.logo} />
       </div>
       {/* Close Button */}
       <button onClick={onClose} className={styles.closeButton}>
@@ -17,7 +27,7 @@ const SideNav = ({ menuItems, logo, onMenuItemClick, onClose }) => {
             <li key={index} className={styles.navItem}>
               <button
                 className={styles.navLink}
-                onClick={() => onMenuItemClick(item)} // Call the passed function
+                onClick={() => handleMenuItemClick(item)} // Use the new handler
               >
                 {item.icon && <span className={styles.icon}>{item.icon}</span>}
                 <span className={styles.linkText}>{item.name}</span>
